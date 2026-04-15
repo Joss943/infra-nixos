cdf8cf5be2bdf938a007dea04225dcbd5e425f8a
Mickael Ribeiro-Alves <mickael.ribeiro-alves@ac-versailles.fr>
2026-04-15 14:41:31 +0200

    Fix install script PATH

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@ set -euo pipefail
 
 echo "🚀 Installation automatique NixOS"
 
-DISK="$(lsblk -dpno NAME,TYPE | awk '$2=="disk"{print $1}' | grep -vE '/dev/sr|/dev/loop' | head -n1)"
+DISK="$(
+  /run/current-system/sw/bin/lsblk -dpno NAME,TYPE \
+  | /run/current-system/sw/bin/awk '$2=="disk"{print $1}' \
+  | /run/current-system/sw/bin/grep -vE '/dev/sr|/dev/loop' \
+  | /run/current-system/sw/bin/head -n1
+)"
 
 if [ -z "${DISK:-}" ]; then
   echo "No target disk found."
@@ -13,10 +18,10 @@ fi
 echo "Disque détecté : $DISK"
 sleep 3
 
-parted -s "$DISK" -- mklabel gpt
-parted -s "$DISK" -- mkpart ESP fat32 1MiB 512MiB
-parted -s "$DISK" -- set 1 esp on
-parted -s "$DISK" -- mkpart primary ext4 512MiB 100%
+/run/current-system/sw/bin/parted -s "$DISK" -- mklabel gpt
+/run/current-system/sw/bin/parted -s "$DISK" -- mkpart ESP fat32 1MiB 512MiB
+/run/current-system/sw/bin/parted -s "$DISK" -- set 1 esp on
+/run/current-system/sw/bin/parted -s "$DISK" -- mkpart primary ext4 512MiB 100%
 
 if [[ "$DISK" =~ nvme ]]; then
   P1="${DISK}p1"
@@ -26,14 +31,14 @@ else
   P2="${DISK}2"
 fi
 
-mkfs.vfat -F 32 "$P1"
-mkfs.ext4 -F "$P2"
+/run/current-system/sw/bin/mkfs.vfat -F 32 "$P1"
+/run/current-system/sw/bin/mkfs.ext4 -F "$P2"
 
-mount "$P2" /mnt
-mkdir -p /mnt/boot
-mount "$P1" /mnt/boot
+/run/current-system/sw/bin/mount "$P2" /mnt
+/run/current-system/sw/bin/mkdir -p /mnt/boot
+/run/current-system/sw/bin/mount "$P1" /mnt/boot
 
-nixos-install --no-root-passwd --flake github:ribmic21-cloud/infra-nixos#nix-admin
+/run/current-system/sw/bin/nixos-install --no-root-passwd --flake github:ribmic21-cloud/infra-nixos#nix-admin
 
 echo "✅ Installation terminée"
-reboot
+/run/current-system/sw/bin/reboot
