95e31abff0d83dfc18a3a3c9e360b21c53a94bef
Mickael Ribeiro-Alves <mickael.ribeiro-alves@ac-versailles.fr>
2026-04-15 10:53:43 +0200

    Fix GNOME options and locale

--- modules/common.nix
+++ modules/common.nix
@@ -14,7 +14,12 @@
   ];
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.xserver.videoDrivers = [ "fbdev" ];
+  
+  i18n.defaultLocale = "fr_FR.UTF-8";
 }
+
+
+
