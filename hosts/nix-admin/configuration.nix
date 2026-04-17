8559d9d611b3ec9061284c777db320f1c9d8d0a9
Mickael Ribeiro-Alves <mickael.ribeiro-alves@ac-versailles.fr>
2026-04-17 15:43:07 +0200

    Ajout applications + outils AD (sans config domaine)

--- hosts/nix-admin/configuration.nix
+++ hosts/nix-admin/configuration.nix
@@ -9,7 +9,34 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # À changer machine par machine plus tard
   networking.hostName = "nix-admin";
+
+  # Applications + outils nécessaires pour une future intégration AD
+  environment.systemPackages = with pkgs; [
+    libreoffice
+    nextcloud-client
+    thunderbird
+
+    adcli
+    oddjob
+    samba4Full
+    sssd
+    krb5
+    realmd
+  ];
+
+  # Compte local temporaire
+  users.users.admin = {
+    isNormalUser = true;
+    description = "Admin local";
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "admin";
+  };
+
+  security.sudo.wheelNeedsPassword = true;
+
+  networking.networkmanager.enable = true;
 
   system.stateVersion = "25.11";
 }
