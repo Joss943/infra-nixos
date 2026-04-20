311222b482629f999273b7f1a3203d5a0b1ec5d1
Mickael Ribeiro-Alves <mickael.ribeiro-alves@ac-versailles.fr>
2026-04-20 13:22:37 +0200

    Ajout applications et outils AD sur nix-auto v6

--- hosts/nix-auto/configuration.nix
+++ hosts/nix-auto/configuration.nix
@@ -11,5 +11,47 @@
 
   networking.hostName = "nix-auto";
 
+  environment.systemPackages = with pkgs; [
+    libreoffice
+    nextcloud-client
+    thunderbird
+
+    adcli
+    oddjob
+    samba4Full
+    sssd
+    krb5
+    realmd
+  ];
+
+  # Prépare Kerberos sans mettre ton vrai domaine dans Git
+  security.krb5.enable = true;
+
+  # Création auto du home au premier login
+  security.pam = {
+    makeHomeDir.umask = "077";
+    services.login.makeHomeDir = true;
+    services.sshd.makeHomeDir = true;
+  };
+
+  # Recommandé avec SSSD
+  services.nscd = {
+    enable = true;
+    config = ''
+      server-user nscd
+      enable-cache hosts yes
+      positive-time-to-live hosts 0
+      negative-time-to-live hosts 0
+      shared hosts yes
+      enable-cache passwd no
+      enable-cache group no
+      enable-cache netgroup no
+      enable-cache services no
+    '';
+  };
+
+  # Active SSSD, mais sans config de domaine stockée dans le repo public
+  services.sssd.enable = true;
+
   system.stateVersion = "25.11";
 }
--- roles/workstation.nix
+++ roles/workstation.nix
@@ -1,53 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  networking.networkmanager.enable = true;
-
-  users.users.admin = {
-    isNormalUser = true;
-    description = "Admin local";
-    extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "admin";
-  };
-
-  security.sudo.wheelNeedsPassword = true;
-
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-
-    libreoffice
-    nextcloud-client
-    thunderbird
-
-    adcli
-    oddjob
-    samba4Full
-    sssd
-    krb5
-    realmd
+  imports = [
+    ../modules/common.nix
+    ../modules/users.nix
+    ../modules/desktop.nix
   ];
-
-  security.krb5.enable = true;
-
-  security.pam = {
-    makeHomeDir.umask = "077";
-    services.login.makeHomeDir = true;
-    services.sshd.makeHomeDir = true;
-  };
-
-  services.nscd = {
-    enable = true;
-    config = ''
-      server-user nscd
-      enable-cache hosts yes
-      positive-time-to-live hosts 0
-      negative-time-to-live hosts 0
-      shared hosts yes
-      enable-cache passwd no
-      enable-cache group no
-      enable-cache netgroup no
-      enable-cache services no
-    '';
-  };
 }
