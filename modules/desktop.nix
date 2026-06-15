add958aea857bd510c2effb61b3ed4dbc8123d20
Josselin Lerendu <josselin.lerendu.pro91@gmail.com>
2026-06-15 15:02:21 +0200

    Replace GNOME with KDE Plasma 6

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,10 +2,26 @@
 
 {
   services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
     layout = "fr";
+    variant = "";
   };
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.kate
+    kdePackages.ark
+    kdePackages.dolphin
+    kdePackages.konsole
+    kdePackages.kscreen
+    kdePackages.plasma-nm
+    kdePackages.plasma-pa
+  ];
 }
